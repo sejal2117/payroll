5d0b22c61def4336911ee0aeb796ffdc0c4cfa53
sejal2117 <121280940+sejal2117@users.noreply.github.com>
2023-07-17 11:31:35 +0530

    Add files via upload

---
+++ salary.sql
@@ -0,0 +1,51 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Nov 04, 2022 at 05:35 PM
+-- Server version: 10.4.21-MariaDB
+-- PHP Version: 8.0.11
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `salary`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `calcul`
+--
+-- Error reading structure for table salary.calcul: #1932 - Table 'salary.calcul' doesn't exist in engine
+-- Error reading data for table salary.calcul: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `salary`.`calcul`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `employee`
+--
+-- Error reading structure for table salary.employee: #1932 - Table 'salary.employee' doesn't exist in engine
+-- Error reading data for table salary.employee: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `salary`.`employee`' at line 1
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `signup`
+--
+-- Error reading structure for table salary.signup: #1932 - Table 'salary.signup' doesn't exist in engine
+-- Error reading data for table salary.signup: #1064 - You have an error in your SQL syntax; check the manual that corresponds to your MariaDB server version for the right syntax to use near 'FROM `salary`.`signup`' at line 1
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
